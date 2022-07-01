ad43de8eb7e7a98919dac9a69037f47d393e6b58
Tejas Bishnoi <tjbishnoi29@gmail.com>
2022-07-01 19:27:29 +0530

    Added files

---
+++ PortPholio/PortPholioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortPholioApp.swift
+//  PortPholio
+//
+//  Created by Tejas Bishnoi on 01/07/22.
+//
+
+import SwiftUI
+
+@main
+struct PortPholioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
